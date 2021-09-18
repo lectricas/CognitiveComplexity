298f941b64df0fc354d7f8d0c355448126fc9815
GitHub Action <action@github.com>
2021-09-18 16:37:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lectricas/cognitivecomplexity/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lectricas.cognitivecomplexity.services
+
+import com.github.lectricas.cognitivecomplexity.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/lectricas/cognitivecomplexity/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lectricas/cognitivecomplexity/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lectricas.cognitivecomplexity.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lectricas.cognitivecomplexity.MyBundle
 
 class MyProjectService(project: Project) {
 
